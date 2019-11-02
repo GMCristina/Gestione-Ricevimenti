0c6c9bfcb1c629f5cda09f76e8cc3ab28fe5e3b0
GMCristina <giannini.m.cristina@gmail.com>
2019-11-02 21:52:21 +0100

    Bozza LoginPage

--- Gestione Ricevimenti/Gestione Ricevimenti/App.xaml.cs
+++ Gestione Ricevimenti/Gestione Ricevimenti/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Gestione_Ricevimenti
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
